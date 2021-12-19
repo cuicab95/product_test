add5718315be619bcb47f480ca27f0bdb8588010
cuicab <jcarlosuicab1995@gmail.com>
2021-12-19 17:28:25 -0600

    Doc swagger.

--- product/apps/store/api/views.py
+++ product/apps/store/api/views.py
@@ -1,3 +1,5 @@
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
@@ -13,6 +15,12 @@ class CreateOrderView(APIView):
     """
     serializer_class = CreateOrderSerializer
 
+    id_param = openapi.Parameter('id', openapi.IN_QUERY, description="Id del pedido", type=openapi.TYPE_INTEGER)
+    is_urgent_param = openapi.Parameter('is_urgent', openapi.IN_QUERY, description="Obtener pedidos urgentes", type=openapi.TYPE_BOOLEAN)
+    order_type_param = openapi.Parameter('order_type', openapi.IN_QUERY, description="Tipo de pedido", type=openapi.TYPE_STRING)
+    customer_type_param = openapi.Parameter('customer_type', openapi.IN_QUERY, description="Tipo de cliente", type=openapi.TYPE_STRING)
+    is_assortment_param = openapi.Parameter('is_assortment', openapi.IN_QUERY, description="Obtener pedidos que no se han surtido", type=openapi.TYPE_BOOLEAN)
+    @swagger_auto_schema(operation_description="API para el listado de productos", manual_parameters=[id_param, is_urgent_param, order_type_param, customer_type_param, is_assortment_param], responses={200: 'Success', 400: 'Bad request', 500: 'External error'})
     def get(self, request):
         """
         API para el listado de productos
@@ -44,8 +52,17 @@ class CreateOrderView(APIView):
             data = OrderSerializer(queryset, many=True).data
             return Response({'data': data}, status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+    customer_id_param = openapi.Parameter('customer_id', openapi.IN_QUERY, description="Id del cliente", type=openapi.TYPE_INTEGER)
+    is_urgent_param = openapi.Parameter('is_urgent', openapi.IN_QUERY, description="Es pedido urgente", type=openapi.TYPE_BOOLEAN)
+    type_param = openapi.Parameter('type', openapi.IN_QUERY, description="Tipo de pedido", type=openapi.TYPE_STRING)
+    items_param = openapi.Parameter('items', openapi.IN_QUERY, description="Listado de productos, por ejemplo: [{'product_id': 1, 'quantity': 2}]", type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_INTEGER))
+    additional_data_param = openapi.Parameter('additional_data', openapi.IN_QUERY, description="Información adicional, por ejemplo: {'stock': 'Almacen 1'}", type=openapi.TYPE_OBJECT)
+
+    @swagger_auto_schema(operation_description="API para crear un nuevo pedido",
+                         manual_parameters=[customer_id_param, is_urgent_param, type_param, items_param, additional_data_param],
+                         responses={201: 'Success', 400: 'Bad request', 500: 'External error', 404: 'Not found'})
     def post(self, request):
         """
         API para crear un nuevo pedido
@@ -84,6 +101,6 @@ class CreateOrderView(APIView):
                 data = OrderSerializer(new_order, many=False).data
                 return Response({'data': data}, status=status.HTTP_201_CREATED)
             except Exception as e:
-                return Response({'message': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+                return Response({'message': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
